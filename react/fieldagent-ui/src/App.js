505ffe0ae62a605d00fcdd0976959b3a546d2019
see-k <flabioski@gmail.com>
2021-05-17 01:06:24 -0500

    Update

--- react/fieldagent-ui/src/App.js
+++ react/fieldagent-ui/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootswatch/dist/cyborg/bootstrap.min.css'; // Added this :boom:
 import './App.css';
 import FieldAgent from './components/FieldAgent';
-import Agents from './components/AgentList';
+import AgentList from './components/AgentList';
 import AddAgent from './components/AddAgent';
 import UpdateAgent from './components/UpdateAgent';
 import RemoveAgent from './components/RemoveAgent';
@@ -17,13 +17,27 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import AgentList from './components/AgentList';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import jwt_decode from 'jwt-decode';
 
 function App() {
 
   const [user, setUser] = useState(null);
+
+  const [fieldAgents, setFieldAgents] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/agent")
+        .then(response => {
+            if (response.status !== 200) {
+                return Promise.reject("get didn't work ...");
+            }
+            return response.json();
+        })
+        .then(json => setFieldAgents(json))
+        .catch(console.log("oops..."));
+}, []);
+
+
 
   const login = (token) => {
     const {id, sub: username, roles: rolesString } = jwt_decode(token);
@@ -80,28 +94,33 @@ function App() {
         <Router>
           <ul className="nav nav-tabs">
             <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="tab" href="#home">
+              <a className="nav-link" data-bs-toggle="tab">
                 <Link to="/">Home</Link>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="tab" href="#Agents">
+              <a className="nav-link" data-bs-toggle="tab">
                 <Link to="/agents">Agents</Link>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="tab" href="#addAgent">
+              <a className="nav-link" data-bs-toggle="tab">
                 <Link to="/agents/add">Add Agent</Link>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="tab" href="#editAgent">
+              <a className="nav-link" data-bs-toggle="tab" >
                 <Link to="/agents/edit/:id">Edit Agent</Link>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="tab" href="#deleteAgent">
+              <a className="nav-link" data-bs-toggle="tab">
                 <Link to="/agents/delete/:id">Delete Agent</Link>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-bs-toggle="tab">
+                <Link to="/login" onClick={logout}>Log out</Link>
               </a>
             </li>
           </ul>
@@ -129,7 +148,7 @@ function App() {
               <RemoveAgent />
             </Route>
             <Route path="/agents">
-              <Agents />
+              <AgentList agents={fieldAgents} />
             </Route>
             <Route path="/login">
               <Login />
